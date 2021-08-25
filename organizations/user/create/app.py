8c569c4eefa2f7d01071a3fa662dd9dc7a45a8e6
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-08-25 17:00:21 +0200

    Modify template and add dummy lambda handler

---
+++ organizations/user/create/app.py
@@ -0,0 +1,13 @@
+import json
+
+
+def lambda_handler(event, context):
+    body = json.loads(event['body'])
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'success!',
+            'body': body
+        })
+    }
