78f73af760cea36afd3cbefcfbe8ed8f66e18e51
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-08-25 16:21:23 +0200

    Add db client

---
+++ organizations/user/create/util/db.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+from organizations.user.create.util.aws import get_db_credentials
+
+
+class MongoDBConnection:
+    def __init__(self):
+        self.connection = None
+
+    def __enter__(self):
+        credentials = get_db_credentials()
+        username = credentials['username']
+        password = credentials['password']
+        host = credentials['host']
+        dbname = credentials['dbname']
+
+        self.connection = MongoClient(f'mongodb+srv://{username}:{password}@{host}/{dbname}')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
